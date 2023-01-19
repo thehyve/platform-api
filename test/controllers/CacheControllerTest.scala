427964ec9698778302c7e0901c31156d6ed4f1c5
Fedde Schaeffer <fedde@thehyve.nl>
2023-01-19 17:24:07 +0100

    Sketch an integrative test of the cache reset

--- test/controllers/CacheControllerTest.scala
+++ test/controllers/CacheControllerTest.scala
@@ -5,25 +5,33 @@ import java.util.concurrent.TimeUnit
 import akka.util.Timeout
 import controllers.api.v4.graphql.GraphQLController
 import controllers.api.v4.rest.CacheController
-import models.gql.Fetchers
+import javax.inject.Inject
+import models.Backend
+import models.entities.Drug
 import org.scalatest.concurrent.ScalaFutures
+import org.scalatest.TestData
 import org.scalatestplus.play.PlaySpec
-import org.scalatestplus.play.guice.GuiceOneAppPerSuite
+import org.scalatestplus.play.guice.GuiceOneAppPerTest
+import play.api.cache.AsyncCacheApi
+import play.api.db.slick.DatabaseConfigProvider
+import play.api.inject.bind
 import play.api.inject.guice.GuiceApplicationBuilder
 import play.api.libs.json.Json
 import play.api.mvc.Result
 import play.api.mvc.Results.Forbidden
-import play.api.test.Helpers.{GET, POST}
+import play.api.test.Helpers.{contentAsString, GET, POST}
 import play.api.test.{FakeRequest, Injecting}
-import play.api.{Application, Logging}
+import play.api.{Application, Configuration, Environment, Logging}
+import play.db.NamedDatabase
+import scala.concurrent.{Future, ExecutionContext}
 import test_configuration.IntegrationTestTag
 
-import scala.concurrent.Await
+import scala.concurrent.{Await, Future}
 import scala.concurrent.duration.DurationInt
 
 class CacheControllerTest
     extends PlaySpec
-    with GuiceOneAppPerSuite
+    with GuiceOneAppPerTest
     with Injecting
     with Logging
     with ScalaFutures {
@@ -32,19 +40,18 @@ class CacheControllerTest
   lazy val gqlController: GraphQLController = inject[GraphQLController]
   val apiKey = "superSecret"
 
-  override def fakeApplication(): Application =
+  override def newAppForTest(td: TestData): Application = {
     new GuiceApplicationBuilder()
       .configure(Map("ot.apiKeyHash" -> apiKey.hashCode.toString))
+      .overrides(bind[Backend].to[BreakableEsBackend])
       .build()
+  }
 
   implicit lazy val timeout: Timeout = Timeout(1120, TimeUnit.SECONDS)
 
-  "A cache" must {
-    "be cleared" taggedAs IntegrationTestTag in {
-
-      // given: a seeded cache
-      val drugCache = Fetchers.drugsFetcherCache
-
+  "A cached request" must {
+    "not need the database again if re-requested" taggedAs IntegrationTestTag in {
+      // given: a cached request
       val query = Json.parse(
         """{ "query": "query { drugs(chemblIds: [\"CHEMBL221959\", \"CHEMBL2103743\"]) { id } }"}"""
       )
@@ -52,14 +59,29 @@ class CacheControllerTest
         .withHeaders(("Content-Type", "application/json"))
         .withBody(query)
       Await.result(gqlController.gqlBody().apply(request), 10.second)
-      // check there is something in the cache
-      assert(drugCache.get("CHEMBL221959").isDefined)
+      // when: the request is repeated while the db is down
+      val resultF: Future[Result] = gqlController.gqlBody().apply(request)
+      // then: the response comes back without issue
+      val resultHasErrors = contentAsString(resultF)(10.second).contains("errors")
+      resultHasErrors mustBe false
+    }
+    "need the database again if the cache has been cleared" taggedAs IntegrationTestTag in {
+      // given: a cached request
+      val query = Json.parse(
+        """{ "query": "query { drugs(chemblIds: [\"CHEMBL221959\", \"CHEMBL2103743\"]) { id } }"}"""
+      )
+      val request = FakeRequest(POST, "/graphql")
+        .withHeaders(("Content-Type", "application/json"))
+        .withBody(query)
+      Await.result(gqlController.gqlBody().apply(request), 10.second)
 
-      // when: the cache is cleared
+      // when: the cache is cleared before repeating while the db is down
       val clearRequest = FakeRequest(GET, "/cache/clear").withHeaders(("apiKey", apiKey))
       Await.result(controller.clearCache().apply(clearRequest), 2.second)
-      // then: the cache is empty
-      drugCache.get("CHEMBL221959").isDefined mustBe false
+      val resultF: Future[Result] = gqlController.gqlBody().apply(request)
+      // then: the response reports an issue
+      val resultHasErrors = contentAsString(resultF)(10.second).contains("errors")
+      resultHasErrors mustBe true
     }
   }
   "A request to clear the cache" must {
@@ -86,6 +108,26 @@ class CacheControllerTest
       // then
       result.header.status mustEqual Forbidden.header.status
 
+    }
+  }
+}
+
+class BreakableEsBackend @Inject() (implicit
+    ec: ExecutionContext,
+    @NamedDatabase("default") dbConfigProvider: DatabaseConfigProvider,
+    config: Configuration,
+    env: Environment,
+    cache: AsyncCacheApi
+) extends Backend {
+
+  var hasHandledARequest: Boolean = false
+
+  override def getDrugs(ids: Seq[String]): Future[IndexedSeq[Drug]] = {
+    val firstRequest = !hasHandledARequest
+    hasHandledARequest = true
+    firstRequest match {
+      case true => super.getDrugs(ids: Seq[String])
+      case false => Future.failed(new com.sksamuel.elastic4s.http.JavaClientExceptionWrapper(new Exception("Faking a broken database in a test")))
     }
   }
 }
